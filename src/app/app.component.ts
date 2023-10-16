065df5bea814b44dd7e234c23b6108dbd1cfb8ac
Bharath Nair <nairbharath007@gmail.com>
2023-10-17 00:33:31 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StudentService } from './services/student.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'services-demo';
+  title = 'Welcome to Student API';
+  studentData: any
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+
+  constructor(private studentInfo: StudentService, private router: Router) {
+    studentInfo.getAllStudents().subscribe((data) => {
+      this.studentData = data;
+      console.log(this.studentData)
+    })
+  }
+
+  updateStudent() {
+    this.router.navigateByUrl('/update')
+  }
+  addStudent() {
+    this.router.navigateByUrl('/add')
+  }
+  deleteStudent() {
+    this.router.navigateByUrl('/remove')
+  }
+
+  searchStudents() {
+    this.router.navigateByUrl('/search'); // Add a route for the search component
+  }
 }
